a94d624d0d119fd99737f883a9f0489657867c0b
Stephen Sennett <ssennett@ssennett.net>
2025-10-05 15:03:04 +1100

    feat: improving build script

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 # Builds and uploads the artifacts for the backend S3 repository.
 # Not required for workshop usage - hosting only.
 
+set -e
+
+echo "🛠️ Deploying artifacts to S3..."
+
 # Lambda Packages
 cd ./src/lambda
 zip \
@@ -18,3 +22,5 @@ aws s3 cp \
     ./src/templates/cloudformation.yaml \
     s3://ssennett-lab-persistent/2025/eventdrivenimageprocessing/cloudformation.yaml \
     --profile labartifacts
+
+echo "✅ Deployment complete."
